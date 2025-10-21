b6f761f304858f4e438b28a7c45462544dea1114
Attilam21 <attiliomazzetti@gmail.com>
2025-10-21 02:57:44 +0200

    Fix all errors: Clean up pages/index.js exports, ensure all Firebase functions deployed, verify build and hosting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,2 @@
 export { default as Home } from './Home';
-export { default as Dashboard } from './Dashboard';
 export { default as Login } from './Login';
-export { default as Rosa } from './Rosa';
-export { default as Match } from './Match';
-export { default as MatchOCR } from './MatchOCR';
-export { default as Statistiche } from './Statistiche';
-export { default as Avversario } from './Avversario';
